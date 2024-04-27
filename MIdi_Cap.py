eb8f31553d55fc8231bec1482f5bad8ac797e896
Chiu_yeer <32241150+Chiuqyan@users.noreply.github.com>
2024-04-27 18:00:46 +0800

    Update MIdi_Cap.py

--- MIdi_Cap.py
+++ MIdi_Cap.py
@@ -44,7 +44,7 @@ class TextAndMIDIModel(pl.LightningModule):
         
         self.tokenizer = tokenizer
         self.midi_model = MIDIModel(tokenizer)
-        self.midi_model.load_state_dict(torch.load('Yeerchiu/midi-llama-test1/model.ckpt'), strict=False)# midi ckpt path here
+        self.midi_model.load_state_dict(torch.load('Yeerchiu/MIDI-TEXT/model.ckpt'), strict=False)# midi ckpt path here
         for param in self.midi_model.parameters():  # frozen
             param.requires_grad = False
         self.text_model = GPT2LMHeadModel.from_pretrained("openai-community/gpt2")
